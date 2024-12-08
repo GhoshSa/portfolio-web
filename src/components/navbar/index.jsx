7646011a4f52c743442fb94cbc689690968c57c0
Sayantan Ghosh <ghoshsayantan710@gmail.com>
2024-12-08 21:36:36 +0530

    Designed and implented the Navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,21 +1,35 @@
-import React from "react"
 import "./navbar.css"
 import { tabs } from "../../sources"
 import { Link } from "react-scroll"
+import Logo from "../../common/logo"
+import { useState } from "react"
+import { HiMenu } from "react-icons/hi"
+import { FaTimes } from "react-icons/fa"
 
 const Navbar = () => {
+    const [openSidebar, setOpenSidebar] = useState(false)
     return (
         <navbar className="navbar flex">
-            <div className="box flex-center tabs-group sidebar">
-                { tabs.map((tab, index) => (
-                        <Link to={ tab.id } smooth={ true } spy={ true } className="tab" activeClass="active" key={ index }>
+            { openSidebar ? <div className="overlay" onClick={() => setOpenSidebar(!openSidebar)}/> : "" }
+            <Logo/>
+            <div className={ `box flex-center tabs-group sidebar ${ openSidebar ? "visible" : "" }` }>
+                <div className="flex-center icon-wrapper cancel-btn" onClick={() => setOpenSidebar(!openSidebar)}>
+                    <FaTimes/>
+                </div>
+                {
+                    tabs.map((tab, index) => (
+                        <Link to={ tab.id } smooth={ true } spy={ true } className="tab" activeClass="active" key={ index } onClick={() => setOpenSidebar(false)}>
                             { tab.name }
                         </Link>
                     ))
                 }
             </div>
-            <div>
-                
+            <div className="box flex-center buttons">
+                <Link to="contact" smooth={ true } className="btn primary contact-btn">Hire me</Link>
+                <Link to="services" smooth={ true } className="btn services-btn">Services</Link>
+                <div className="flex-center icon-wrapper menu-btn" onClick={() => setOpenSidebar(!openSidebar)}>
+                    <HiMenu/>
+                </div>
             </div>
         </navbar>
     )
