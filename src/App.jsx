388edfd7dac63a2a23be6fe153d12506c8fac6c3
Sayantan Ghosh <ghoshsayantan710@gmail.com>
2024-12-08 16:10:37 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import About from "./components/about"
+import Contact from "./components/contacts"
+import Footer from "./components/footer"
+import Hero from "./components/hero"
 import Navbar from "./components/navbar"
+import Projects from "./components/projects"
+import Services from "./components/services"
+import Skill from "./components/skill"
+import Testimonials from "./components/testimonials"
 
 function App() {
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Skill/>
+      <Services/>
+      <Projects/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
+    </>
   )
 }
 
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,11 +1,23 @@
 import React from "react"
 import "./navbar.css"
+import { tabs } from "../../sources"
+import { Link } from "react-scroll"
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <navbar className="navbar flex">
+            <div className="box flex-center tabs-group sidebar">
+                { tabs.map((tab, index) => (
+                        <Link to={ tab.id } smooth={ true } spy={ true } className="tab" activeClass="active" key={ index }>
+                            { tab.name }
+                        </Link>
+                    ))
+                }
+            </div>
+            <div>
+                
+            </div>
+        </navbar>
     )
 }
 
